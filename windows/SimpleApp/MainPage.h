df37283edff5fd30948c00697332a5323bcb7acd
MdSalman2022 <salman15-4523@diu.edu.bd>
2022-12-22 01:58:46 +0600

    hello world by react native

---
+++ windows/SimpleApp/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::SimpleApp::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::SimpleApp::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
